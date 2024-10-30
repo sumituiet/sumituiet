76e6e7b05bf324e31d1b7051280fe59fb5c4981e
Sumit <Sumitsainiuiet@gmail.com>
2024-10-31 00:03:54 +1100

    fix link issue

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,19 +4,14 @@ import Background from "./background";
 export default function Contact() {
     return (
         <Background>
-        <Container className="full-page-component" style={
-            {
-                color: 'white',
-                textAlign: 'center',
-                padding: '5rem'
-            }}>
+        <Container className="full-page-component">
         <Motion >
         <h1>Hire Me</h1>
         <p>
         I am currently looking for opportunities as a Machine Learning Engineer or Data Scientist. If you have a role that fits my profile, please get in touch.
         </p>
         <p>
-        Email: <a href="mailto: sumitsainiuiet@gmail.com">Sumit</a>
+        Email: <a href="mailto:sumitsainiuiet@gmail.com">sumitsainiuiet@gmail.com</a>
         </p>
         </Motion>
         </Container>
